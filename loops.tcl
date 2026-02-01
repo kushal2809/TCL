6da1aeb46c00778b79d1d0db33686d42a8755514
Koushik D <101798417+kushal2809@users.noreply.github.com>
2026-02-02 00:57:40 +0530

    Create loops.tcl
    This file contains of the loop concepts ( for, foreach, and while)

---
+++ loops.tcl
@@ -0,0 +1,46 @@
+puts {for, foreach, while }
+set for {we use for loop for the known valus like we know the staring point, ending point, and inrement then we use for loop}
+set syntax {for {start point} {condition} {increment}{
+commands
+}}
+set forex {ex: for {set x 1} {$x <= 5} {incr x}{
+puts $x
+}}
+for {set x 1} {$x <= 5} {incr x} {
+puts $x
+}
+set foreach {we use foreach loop for the know number of list like if we have a set of list to loop them we use the foreach}
+set fesyntax {foreach var list {
+command
+}}
+set feex {ex: set y {a b c}
+foreach z $y {
+puts $z
+}}
+set y {a b c}
+foreach z $y {
+puts $z
+}
+set while {we use while loop when we know the exact condition but dont know how many times we need to execute the loop}
+set whilesyntax {while {condition} {
+command
+}}
+set whileex {ex: set i 1
+while {$i <= 3} {
+    puts $i
+    incr i
+}}
+set i 1
+while {$i <= 3} {
+    puts $i
+    incr i
+}
+
+puts $for
+puts $forex
+puts $foreach
+puts $fesyntax
+puts $feex
+puts $while
+puts $whilesyntax
+puts $whileex
